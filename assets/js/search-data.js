1187119e44cf6d07e4513372abb744ff98deedc6
wenhq123 <40376331+wenhq123@users.noreply.github.com>
2026-03-10 00:55:08 +0000

    Deploying to gh-pages from @ wenhq123/wenhq123.github.io@1824348f360872ca5f8382240aa7225ac93fe836 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-teaching",
-          title: "Teaching",
+        },{id: "nav-teaching-experience",
+          title: "Teaching Experience",
           description: "",
           section: "Navigation",
           handler: () => {
