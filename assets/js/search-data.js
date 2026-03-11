48dc2bdb678c3fe85aae17676c19e85fce3010d2
wenhq123 <40376331+wenhq123@users.noreply.github.com>
2026-03-11 23:53:53 +0000

    Deploying to gh-pages from @ wenhq123/wenhq123.github.io@128aa5dee232e25aa9d9e0417474fde80f614744 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,18 +28,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
